e9ff5c3c0bda912917759bb762906cb12da4b247
jeggonlinellc <jeggonlinellc@gmail.com>
2021-03-04 08:28:45 -0800

    advanced react topics

---
+++ src/hooks/Counter.jsx
@@ -0,0 +1,24 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+function Counter(props) {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    document.title = `${name}has clicked ${count} times!`;
+    return () => {
+      console.log("Clean up");
+    };
+  }, []);
+  return (
+    <Fragment>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <div>
+        {name} has clicked {count} times!
+      </div>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </Fragment>
+  );
+}
+
+export default Counter;

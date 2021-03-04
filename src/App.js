3f432f99f3ee0f316a6cb0799dcc37e2c019a0e6
jeggonlinellc <jeggonlinellc@gmail.com>
2021-03-04 14:34:38 -0800

    fetched data with axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
 import Counter from "./hooks/Counter";
+import Users from "./hooks/Users";
 
 function App(props) {
-  return <Counter />;
+  return <Users />;
 }
 
 export default App;
